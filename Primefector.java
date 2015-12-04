b79376fe580f8d68b9822198bea36a88038a39b0
napawan410 <napawan410@hotmail.com>
2015-12-04 11:39:40 +0700

    Update Primefector.java

--- Primefector.java
+++ Primefector.java
@@ -7,19 +7,15 @@ public class Primefactor {
     private static void getPrime(int inputnumber) {
              
            for (int i = 2; i <=inputnumber; i++) {
-                
                  while (inputnumber % i == 0) {
-                        
-                         inputnumber = inputnumber / i;
-                         System.out.print(i);
-                         
+                    inputnumber = inputnumber / i;
+                    System.out.print(i);
                          if (inputnumber >= i) { 
                            System.out.print(" x ");
-                       }
-                }
+                         }
+                 }
             }
-            
-         }
+    }
     public static void main(String[] args) {
         int inputnumber;
         Scanner in = new Scanner(System.in);
